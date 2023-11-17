aa577cfc9226668fcc4e313b4c6299d669e127ae
Juan <zerodarkzone@gmail.com>
2023-11-16 22:47:18 -0500

    Solve Q3 with polars

--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,16 @@
 from typing import List, Tuple
+import polars as pl
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    # Read the json file as a polars DataFrame
+    data: pl.LazyFrame = pl.scan_ndjson(file_path)
+
+    # Get user mentions
+    mentions = data.select(
+        pl.col("mentionedUsers").explode().struct.field("username").alias("username")
+    ).filter(pl.col("username").is_not_null())
+
+    # Get the top 10 users
+    top_mentions = mentions.group_by("username").count().sort("count", descending=True).head(10)
+
+    return top_mentions.collect().rows()
