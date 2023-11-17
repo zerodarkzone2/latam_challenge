35f4d3aa69f9d6141cca5ad733737a1e794c9ea1
Juan <zerodarkzone@gmail.com>
2023-11-16 23:41:16 -0500

    Solve Q2 with python Counter

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,17 @@
 from typing import List, Tuple
+from collections import Counter
+import emoji
+import json
 
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    emoji_counter = Counter()
+    with open(file_path, "r") as jf:
+        for line in jf:
+            row = json.loads(line)
+            content = row["content"]
+            # Extract emojis from text and update counter
+            emoji_counter.update([em.chars for em in emoji.analyze(content)])
+
+    # Get the top 10 emojis
+    top_10_emoji = emoji_counter.most_common(10)
+    return top_10_emoji
